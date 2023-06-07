b425377e3f574f1e9819487c293442283402ee4c
lekdup <lekdup@live.fr>
2023-06-07 16:16:44 +0200

    components created - header, footer, candidat

---
+++ front/src/components/App.js
@@ -0,0 +1,16 @@
+import './App.scss';
+import Header from './Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <article className="intro">
+        <h1>O'Boulot</h1>
+        <p>Pour ceux qui en veulent</p>
+      </article>
+    </div>
+  );
+}
+
+export default App;
---
+++ front/src/components/Header/Profile/Profile.js
@@ -0,0 +1,11 @@
+import './Profile.scss';
+
+function Profile() {
+    return(
+        <section>
+            <h1>O</h1>
+        </section>
+    )
+}
+
+export default Profile;
