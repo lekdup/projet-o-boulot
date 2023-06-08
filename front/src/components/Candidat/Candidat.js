cfddade14b28e5fa97abbdb441ffdf157d6a77c0
lekdup <lekdup@live.fr>
2023-06-08 11:25:08 +0200

    homepage done

--- front/src/components/Candidat/Candidat.js
+++ front/src/components/Candidat/Candidat.js
@@ -2,9 +2,9 @@ import './Candidat.scss';
 
 function Candidat() {
     return(
-        <div className="wrapper">
-            <h2>Je cherche un boulot</h2>
-            <div>
+        <div className="wrapper-candidat">
+            <h2 className="wrapper-candidat-title">Je cherche un boulot</h2>
+            <div className="wrapper-candidat-buttons">
                 <button>Consulter les offres</button>
                 <button>Créer mon espace candidat</button>
             </div>
