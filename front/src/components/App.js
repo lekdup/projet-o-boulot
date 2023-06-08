8ad72ebbb14b7cec6348c3266542a0cafe74358b
3M <3m@MacBook-Pro-de-3M.local>
2023-06-08 14:00:39 +0200

    espace Entreprise mobile

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -2,17 +2,18 @@ import './App.scss';
 import Homepage from './Homepage/Homepage';
 import CandidatArea from './CandidateArea/CandidateArea';
 import { Routes, Route } from 'react-router-dom';
+import EntrepriseArea from './EntrepriseArea/EntrepriseArea';
 
 function App() {
   return (
     <div className="App">
+      
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/candidat" element={<CandidatArea />} />
+        <Route path="/entreprise" element={<EntrepriseArea />} />
+       </Routes>
 
-     <Routes>
-       <Route path="/" element={<Homepage />} />
-       <Route path="/candidate-area" element={<CandidatArea />} />
-
-
-    </Routes>
     </div>
   );
 }
---
+++ front/src/components/EntrepriseArea/EntrepriseArea.js
@@ -0,0 +1,28 @@
+import './EntrepriseArea.scss';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+
+function EntrepriseArea() {
+
+    return(
+        <div className="EntrepriseArea">
+            <Header />
+            
+            <h1 className="EntrepriseArea-title"> Bonjour entreprise X </h1>
+            
+            <div className='EntrepriseArea-buttons'>
+                <button className='EntrepriseArea-button' >Mes données</button>
+                <button className='EntrepriseArea-button' >Mes offres d'emploi</button>
+                <button className='EntrepriseArea-button' >Mes documents</button>
+            </div>
+            
+
+            <Footer />
+
+        </div>
+
+
+    )
+}
+
+export default EntrepriseArea;
