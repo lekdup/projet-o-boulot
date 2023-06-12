6beef7973a4cb8e3fef2aefef0a07a76bb3711da
lekdup <lekdup@live.fr>
2023-06-12 11:36:38 +0200

    hompage static desktop version done

--- front/src/components/Candidat/Candidat.js
+++ front/src/components/Candidat/Candidat.js
@@ -1,9 +1,13 @@
+import apply from '../../assets/apply.svg';
 import './Candidat.scss';
 
 function Candidat() {
     return(
         <div className="wrapper-candidat">
             <h2 className="wrapper-candidat-title">Je cherche un boulot</h2>
+            <div className="wrapper-candidat-logo">
+                <img src={apply} alt="illustration of a man building blocks on a board" />
+            </div>
             <div className="wrapper-candidat-buttons">
                 <button>Consulter les offres</button>
                 <button>Créer mon espace candidat</button>
--- front/src/components/Entreprise/Entreprise.js
+++ front/src/components/Entreprise/Entreprise.js
@@ -1,10 +1,13 @@
-
+import recruit from '../../assets/recruit.svg';
 import './Entreprise.scss';
 
 function Entreprise() {
     return(
         <div className="wrapper-entreprise">
             <h2 className="wrapper-entreprise-title">Je recrute !</h2>
+            <div className="wrapper-entreprise-logo">
+                <img src={recruit} alt="illustration of a man building blocks on a board" />
+            </div>
             <div className="wrapper-entreprise-buttons">
                 <button className='wrapper-entreprise-button'>Créer une offre</button>
                 <button className='wrapper-entreprise-button'>Je souhaite en savoir +</button>
