5371cda416dc7f4a27500bc772b95e645f69d118
lekdup <lekdup@live.fr>
2023-06-12 13:43:28 +0200

    added all the Routes on develop branch App component

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -1,14 +1,45 @@
 import './App.scss';
-import Footer from './Footer/Footer';
-import Header from './Header/Header';
+
+import { Routes, Route } from 'react-router-dom';
+
 import Homepage from './Homepage/Homepage';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+
+import CandidatArea from './CandidateArea/CandidateArea';
+import CandidateDataPage from './CandidateArea/CandidateDataPage/CandidateDataPage';
+import CandidateAlertPage from './CandidateArea/CandidateAlertPage/CandidateAlertPage';
+import CandidateModificationPage from './CandidateArea/CandidateModificationPage/CandidateModificationPage';
+
+import EntrepriseArea from './EntrepriseArea/EntrepriseArea';
+import EntrepriseModificationPage from './EntrepriseArea/EntrepriseModificationPage/EntrepriseModificationPage';
+import EntrepriseDataPage from './EntrepriseArea/EntrepriseDataPage/EntrepriseDataPage';
+import EntrepriseAlertPage from './EntrepriseArea/EntrepriseAlertPage/EntrepriseAlertPage';
+import EntrepriseAddAlertPage from './EntrepriseArea/EntrepriseAddAlertPage/EntrepriseAddAlertPage';
 
 function App() {
+
   return (
     <div className="App">
       <Header />
-      <Homepage />
+      
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+
+        <Route path="/candidat" element={<CandidatArea />} />
+        <Route path="/candidat/mes-donnes" element={<CandidateDataPage />} />
+        <Route path="/candidat/mes-donnes/done" element={<CandidateAlertPage />} />
+        <Route path="/candidat/mes-donnes/modification" element={<CandidateModificationPage />} />
+
+        <Route path="/entreprise" element={<EntrepriseArea />} />
+        <Route path="/entreprise/mes-donnes" element={<EntrepriseDataPage />} />
+        <Route path="/entreprise/mes-donnes/done" element={<EntrepriseAlertPage />} />
+        <Route path="/entreprise/mes-donnes/modification" element={<EntrepriseModificationPage />} />
+        <Route path="/entreprise/ajout-publication/done" element={<EntrepriseAddAlertPage />} />
+      </Routes>
+
       <Footer />
+
     </div>
   );
 }
