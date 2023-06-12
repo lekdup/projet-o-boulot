52bcd0fe5f1d1da28cbdb04e746f4a3c31e13cbb
JulietteGoulam <goulamjuliette@gmail.com>
2023-06-12 16:50:42 +0200

    Article OK

--- front/src/components/Article/Article.js
+++ front/src/components/Article/Article.js
@@ -4,10 +4,10 @@ import article from '../../assets/article.png'
 function Article(){
     return(
         <div className='article'>
-            <button className='article-button'> {'<'} Retour actualités </button>
+            <button className='article-button'> {'<'} Retour Actualités </button>
             <h1 className='article-title'> Du nouveau dans le monde de l'événementiel </h1>
             <div className='article-content'>
-                <img src={article} className='article-content-image' />
+                <img src={article} className='article-content-image' alt=''/>
                 <p className='article-content-text'> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
             </div>
         </div>
