bb90be6b31cdb3f17dd9125a4f27cecc99bf56e8
JulietteGoulam <goulamjuliette@gmail.com>
2023-06-12 16:06:24 +0200

    Actus page OK article in progress

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -2,12 +2,14 @@ import './App.scss';
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
 import Homepage from './Homepage/Homepage';
+import Actus from './Actus/Actus';
+import Article from './Article/Article';
 
 function App() {
   return (
     <div className="App">
       <Header />
-        <Homepage />
+        <Article />
       <Footer />
     </div>
   );
