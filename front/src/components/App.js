b0025c492ef51b0b64f80abd42af661ace9d4099
JulietteGoulam <goulamjuliette@gmail.com>
2023-06-13 14:59:47 +0200

    Tabs Help page OK

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -2,12 +2,13 @@ import './App.scss';
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
 import Homepage from './Homepage/Homepage';
+import Help from './Help/Help';
 
 function App() {
   return (
     <div className="App">
       <Header />
-        <Homepage />
+        <Help />
       <Footer />
     </div>
   );
---
+++ front/src/components/Help/Help.js
@@ -0,0 +1,30 @@
+import './Help.scss';
+
+function Help(){
+    return(
+        <div className='help'>
+            <h1 className='help-title'> Besoin d'aide ?</h1>
+            <div className="help-tabs">
+    <input className="help-tabs"type="radio" id="tabfree" name="mytabs" checked="checked"/> 
+    <label className='help-label blue'for="tabfree">Je suis une entreprise</label>
+    <div className="help-tab blue">
+      <h2>Comment publier une annonce ?</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+
+    <input className="help-tabs"type="radio" id="tabsilver" name="mytabs"/>
+    <label className='help-label orange' for="tabsilver">Je suis un candidat</label>
+    <div className="help-tab orange">
+      <h2>Comment créer mon profil ?</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+    </div>
+
+
+    </div>
+        
+    </div>
+
+    )
+}
+
+export default Help;
