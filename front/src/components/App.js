ebebf749db50babb8e639f13a408cd12c2d00540
lekdup <lekdup@live.fr>
2023-06-13 16:12:27 +0200

    working on different logo color function

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -1,13 +1,18 @@
 import './App.scss';
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
+import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router';
+
 import Homepage from './Homepage/Homepage';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import logoNeutre from '../assets/logo.png';
+import logoEntreprise from '../assets/company.png';
+import logoCandidat from '../assets/candidate.png';
 
 function App() {
-
   const [scrollPosition, setScrollPosition] = useState(0);
+
+  const location = useLocation();
 
     console.log(scrollPosition)
 
@@ -24,11 +29,23 @@ useEffect(() => {
     };
 }, []);
 
+
+const getDifferentLogoColor = () => {
+  if (location.pathname.startsWith('/candidat')) {
+      return logoCandidat;
+  } else if (location.pathname.startsWith('/entreprise')) {
+      return logoEntreprise;
+  } else {
+      return logoNeutre;
+  }
+}
+
+
 console.log(scrollPosition);
 
   return (
     <div className="App">
-      <Header />
+      <Header logoColors={getDifferentLogoColor}/>
       <Homepage />
       <Footer />
     </div>
--- front/src/components/Header/Header.js
+++ front/src/components/Header/Header.js
@@ -1,34 +1,32 @@
+import { useState, useEffect } from 'react';
+
 import './Header.scss';
-import logo from '../../assets/logo.png';
 import Profile from './Profile/Profile';
 import MenuBurger from './MenuBurger/MenuBurger';
 
-import { useState } from 'react';
-import { useEffect } from 'react';
 
-function Header() {
-    
+function Header({ logoColors }) {
     const [scrollPosition, setScrollPosition] = useState(0);
 
     console.log(scrollPosition)
 
     const handleScroll = () => {
-    const position = window.scrollY;
-    setScrollPosition(position);
-};
-
-useEffect(() => {
-    window.addEventListener('scroll', handleScroll, { passive: true });
-
-    return () => {
-        window.removeEventListener('scroll', handleScroll);
+        const position = window.scrollY;
+        setScrollPosition(position);
     };
-}, []);
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll, { passive: true });
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     return (
         <header className={`${scrollPosition > 40 ? "bcg" : ""}`}>
             <div className="logo">
-                <img className="logo-image" src={logo} alt="Logo O'Boulot en couleur Orange" />
+                <img className="logo-image" src={logoColors} alt="Logo O'Boulot en couleur Orange" />
             </div>
             <div className="Menu-profile">
                 <Profile />
