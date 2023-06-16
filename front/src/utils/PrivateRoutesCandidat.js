7ff5f82df3fab2523d78cecf702b02cf7f26e5c4
lekdup <lekdup@live.fr>
2023-06-16 09:06:54 +0200

    test

--- front/src/utils/PrivateRoutesCandidat.js
+++ front/src/utils/PrivateRoutesCandidat.js
@@ -1,12 +1,13 @@
 import { Outlet, Navigate  } from "react-router-dom";
 
 const PrivateRoutesCandidat = () => {
-    let auth = {'token':false}
+    const token = localStorage.getItem('token');
+    
+    const hasToken = token !== null && token !== undefined;
 
-    return(
-        //verifier à quoi correspond le <Outlet/> /!/
-        auth.token ? <Outlet/> : <Navigate to="/candidat/login"/>
-    )
+    return (
+        hasToken ? <Outlet/> : <Navigate to="/candidat/login"/>
+    );
 }
 
 export default PrivateRoutesCandidat;
