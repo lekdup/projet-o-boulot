d0d55f1f684f632f4e9aa29e74ab2055b1ad55ed
JulietteGoulam <goulamjuliette@gmail.com>
2023-06-16 11:37:36 +0200

    test

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -1,5 +1,4 @@
-import { Routes, Route, useLocation} from 'react-router-dom';
-
+import { Routes, Route, useLocation } from 'react-router-dom';
 import './App.scss';
 
 import Header from './Header/Header';
@@ -11,11 +10,7 @@ import Joblist from './Joblist/Joblist';
 import Jobcard from './Jobcard/Jobcard';
 import Contact from './Contact/Contact';
 
-import PrivateRoutesCandidat from '../utils/PrivateRoutesCandidat';
-import PrivateRoutesEntreprise from '../utils/PrivateRoutesEntreprise';
-
 import ConnectionCandidate from './ConnectionCandidate/ConnectionCandidate';
-import CandidateSubscription from './CandidateSubscription/CandidateSubscription';
 import CandidatArea from './CandidateArea/CandidateArea';
 import CandidateDataPage from './CandidateArea/CandidateDataPage/CandidateDataPage';
 import CandidateAlertPage from './CandidateArea/CandidateAlertPage/CandidateAlertPage';
@@ -33,16 +28,6 @@ import Footer from './Footer/Footer';
 import logoNeutre from '../assets/anonyme.png';
 import logoEntreprise from '../assets/company.png';
 import logoCandidat from '../assets/candidate.png';
-import Error404 from './Error404/Error404';
-import CompanySubscription from './CompanySubscription/CompanySubscription';
-
-<<<<<<< HEAD
-=======
-import PrivateRoutesCandidat from '../utils/PrivateRoutesCandidat';
-import PrivateRoutesEntreprise from '../utils/PrivateRoutesEntreprise';
-import Actus from './Actus/Actus';
-import Article from './Article/Article';
->>>>>>> cd2f556d6623fd657d3564e249748e4863af476e
 
 function App() {
   const location = useLocation();
@@ -64,74 +49,29 @@ function App() {
       <Header logoColors={getDifferentLogoColor()}/>
 
       <div className="main-container">
-        
         <Routes>
-<<<<<<< HEAD
 
-        {/* private routes, for only logged in clients */}
-            <Route element={<PrivateRoutesCandidat />}>
-              <Route path="/candidat" element={<CandidatArea />} />
-              <Route path="/candidat/mes-donnes" element={<CandidateDataPage />} />
-              <Route path="/candidat/mes-donnes/done" element={<CandidateAlertPage />} />
-              <Route path="/candidat/mes-donnes/modification" element={<CandidateModificationPage />} />
-=======
-        
-            //ROUTES PRIVES PUBLIC
-            <Route element={<PrivateRoutesCandidat />}>
-              <Route path="/candidat/mes-donnees" element={<CandidateDataPage />} />
-              <Route path="/candidat/mes-donnees/done" element={<CandidateAlertPage />} />
-              <Route path="/candidat/mes-donnees/modification" element={<CandidateModificationPage />} />
-              <Route path="/candidat" element={<CandidatArea />} />
->>>>>>> cd2f556d6623fd657d3564e249748e4863af476e
-            </Route> 
+          <Route path="/" element={<Homepage />} />
+          <Route path="/qui-sommes-nous" element={<AboutUs />} />
+          <Route path="/mentions-legales" element={<Legacy />} /> 
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/aide" element={<Help />} />
 
-            //ROUTES PRIVES ENTREPRISE
-            <Route element={<PrivateRoutesEntreprise />}>
-<<<<<<< HEAD
-              <Route path="/entreprise" element={<EntrepriseArea />} />
-              <Route path="/entreprise/mes-donnes" element={<EntrepriseDataPage />} />
-              <Route path="/entreprise/mes-donnes/done" element={<EntrepriseAlertPage />} />
-              <Route path="/entreprise/mes-donnes/modification" element={<EntrepriseModificationPage />} />
-=======
-              <Route path="/entreprise/mes-donnees" element={<EntrepriseDataPage />} />
-              <Route path="/entreprise/mes-donnees/done" element={<EntrepriseAlertPage />} />
-              <Route path="/entreprise/mes-donnees/modification" element={<EntrepriseModificationPage />} />
->>>>>>> cd2f556d6623fd657d3564e249748e4863af476e
-              <Route path="/entreprise/ajout-publication/done" element={<EntrepriseAddAlertPage />} />
-              <Route path="/entreprise" element={<EntrepriseArea />} />
-            </Route>
+          <Route path="/candidat/login" element={<ConnectionCandidate /> } />
+          <Route path="/candidat" element={<CandidatArea />} />
+          <Route path="/candidat/mes-donnes" element={<CandidateDataPage />} />
+          <Route path="/candidat/mes-donnes/done" element={<CandidateAlertPage />} />
+          <Route path="/candidat/mes-donnes/modification" element={<CandidateModificationPage />} />
+          <Route path="/candidat/joblist" element={<Joblist/>} />
 
-<<<<<<< HEAD
-        {/* public routes, for everybody */}
-=======
-            //ROUTES PUBLIC
->>>>>>> cd2f556d6623fd657d3564e249748e4863af476e
-            <Route path="/" element={<Homepage />} />
-            <Route path="/actualites" element={<Actus />} />
-
-            // a remplacer par l'url suivante: /actualites/:id
-            <Route path="/article" element={<Article />} /> 
-            
-            <Route path="/qui-sommes-nous" element={<AboutUs />} />
-            <Route path="/mentions-legales" element={<Legacy />} /> 
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/aide" element={<Help />} />
-            <Route path="/candidat/login" element={<ConnectionCandidate /> } />
-            <Route path="/candidat/joblist" element={<Joblist/>} />
-<<<<<<< HEAD
-            <Route path="/candidat/inscription" element={<CandidateSubscription />} />
-
-            <Route path="/entreprise/login" element={<ConnectionEntreprise />} />
-            <Route path="/entreprise/inscription" element={<CompanySubscription />} />
-
-            <Route path="*" element={<Error404 />} />
-=======
-            <Route path="/entreprise/login" element={<ConnectionEntreprise />} />
-            
->>>>>>> cd2f556d6623fd657d3564e249748e4863af476e
+          <Route path="/entreprise/login" element={<ConnectionEntreprise />} />
+          <Route path="/entreprise" element={<EntrepriseArea />} />
+          <Route path="/entreprise/mes-donnes" element={<EntrepriseDataPage />} />
+          <Route path="/entreprise/mes-donnes/done" element={<EntrepriseAlertPage />} />
+          <Route path="/entreprise/mes-donnes/modification" element={<EntrepriseModificationPage />} />
+          <Route path="/entreprise/ajout-publication/done" element={<EntrepriseAddAlertPage />} />
 
         </Routes>
-        
       </div>
 
       <Footer />
