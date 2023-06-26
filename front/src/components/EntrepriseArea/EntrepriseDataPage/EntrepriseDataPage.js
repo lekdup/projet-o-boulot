a349f3461c739d5953127295204a4e831e05fdd3
3M <3m@MBP-de-3M.lan>
2023-06-26 11:55:42 +0200

    menu burger fix

--- front/src/components/EntrepriseArea/EntrepriseDataPage/EntrepriseDataPage.js
+++ front/src/components/EntrepriseArea/EntrepriseDataPage/EntrepriseDataPage.js
@@ -1,9 +1,13 @@
 
 import './EntrepriseDataPage.scss';
 import logoEntrepriseDataPage from '../../../assets/logoEntrepriseDataPage.svg'
+import { useSelector } from 'react-redux';
 
 
 function EntrepriseDataPage() {
+
+	const userEntreprise = useSelector(state => state.entreprise.userEntreprise);
+	console.log(userEntreprise);
 
     return(
         <div className="EntrepriseDataPage">
@@ -39,8 +43,8 @@ function EntrepriseDataPage() {
 				<div className="EntrepriseDataPage-p" >Veuillez verifier l'exactitude de vos données personnelles, ces dernière sont très importantes. Ces dernières serviront à établir et à éditer les contrats de travail de vos futurs employés.</div>
 
 				<div className="EntrepriseDataPage-input-buttons">
-						<a href="#" className="EntrepriseDataPage-input-button"  >Modifier</a>
-                        <a href="#" className="EntrepriseDataPage-input-button" >Supprimer</a>
+						<a className="EntrepriseDataPage-input-button" href="/" >Modifier</a>
+                        <a className="EntrepriseDataPage-input-button" href="/" >Supprimer</a>
 				</div>
 
 		</div>
