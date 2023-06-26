85395b235ec3e9dc83ca22e6af5c05b9de744d3c
JulietteGoulam <goulamjuliette@gmail.com>
2023-06-26 14:12:17 +0200

    searchbar for joblist Ok

--- front/src/components/Joblist/Joblist.js
+++ front/src/components/Joblist/Joblist.js
@@ -1,12 +1,31 @@
 import './Joblist.scss';
 import CardsList from './CardsList/CardsList';
-import Jobcard from '../Jobcard/Jobcard';
 import api from '../../api/api';
 import { useEffect, useState } from 'react';
 
 function Joblist(){
 
     const [offers, setOffers] = useState([]);
+    const [searchValue, setSearchValue] = useState('');
+
+    const [searchResult, setSearchResult] = useState([]);
+    const [searchSubmitted, setSearchSubmitted] = useState(false);
+
+
+    const handleChange = (e) => {  
+
+        const search = e.target.value.toLowerCase();
+        setSearchValue(search);
+        console.log(searchValue);
+        
+        const filteredOffers = offers.filter((offer)=> 
+        offer.entitled.toLowerCase().includes(search)
+         );
+        setSearchResult(filteredOffers); 
+        setSearchSubmitted(false);
+
+    }
+    
 
     useEffect(() => {
       api.get("/offres/")
@@ -19,18 +38,29 @@ function Joblist(){
           })
   }, []);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSearchSubmitted(true);
+  };
 
     return(
       
         <div className='Joblist'>
         <h1 className='Joblist-title'> Les offres d'emploi</h1>
-        <form className='Joblist-searchbar'>
-            <input className='Joblist-searchbar-input' type="text" name="searchbar" placeholder="'Hotesse d'accueil' "></input> 
-            <button className='Joblist-searchbar-button'> Recherche </button>
+        <form className='Joblist-searchbar' onSubmit={handleSubmit}>
+            <input 
+            
+            className='Joblist-searchbar-input' 
+            type="text" 
+            name="searchbar" 
+            placeholder="'Hotesse d'accueil' "
+            value={searchValue}
+            onChange={handleChange}></input> 
+            <button type='submit' className='Joblist-searchbar-button'> Recherche </button>
             </form>
         <div className='Joblist-content'>
             
-        <CardsList offers={offers} />
+        <CardsList offers={searchSubmitted ? searchResult : offers} />
         
         </div>
         
