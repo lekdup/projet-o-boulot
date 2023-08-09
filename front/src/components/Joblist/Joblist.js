c2903616e00df5ef7e84b97ca3bd35006f9fee4d
lekdup <lekdup@live.fr>
2023-08-09 12:46:09 +0200

    Joblist all in one file

--- front/src/components/Joblist/Joblist.js
+++ front/src/components/Joblist/Joblist.js
@@ -1,10 +1,15 @@
+import axios from 'axios';
 import './Joblist.scss';
 // import CardsList from './CardsList/CardsList';
 // import api from '../../api/api';
-import { useState } from 'react';
-import JobOfferCard from './JobOfferCard';
+import { useEffect, useState } from 'react';
+// import JobOfferCard from './JobOfferCard';
+import { Link } from 'react-router-dom';
+import { Bars } from 'react-loader-spinner';
 
 function Joblist(){
+
+  const [loading, setLoading] = useState(false);
 
   const [data, setData] = useState([]);
 
@@ -27,6 +32,26 @@ function Joblist(){
         setSearchSubmitted(true);
 
     }
+
+    useEffect(() => {
+      const jobs = "users";
+      const url = new URL(`https://random-data-api.com/api/v2/${jobs}`);
+      let params = new URLSearchParams();
+      params.set("size", 10);
+      params.set("response_type", "json");
+      url.search = params;
+  
+      setLoading(true);
+      axios.get(url)
+      .then(response=>{
+        console.log(response.data)
+        setData(response.data);
+        setTimeout(() => {
+          setLoading(false);
+        }, 2000);
+      })
+      .catch(console.warn);
+    },[])
   //   useEffect(() => {
   //     api.get("/offres/")
   //         .then(res => {
@@ -42,6 +67,18 @@ function Joblist(){
     e.preventDefault();
     // setSearchSubmitted(true);
   };
+
+  if(loading) {
+    return (
+      <div className="loading-container">
+        <Bars
+          width="10rem"
+          height="10rem"
+          color="#FF772B"
+        />
+      </div>
+    )
+  }
 
  const displayedOffers = searchSubmitted ? searchResult : data;
  
@@ -63,7 +100,22 @@ function Joblist(){
         <div >
           <div className='Joblist-content'>
             <p className='Joblist-result'> {displayedOffers.length} résultat{displayedOffers.length > 1 ? 's' : ''}</p>
-            <JobOfferCard setData={setData} data={data} displayedOffers={displayedOffers}/>
+            {displayedOffers.map(({ id, password, last_name, email, avatar, employment }) => (
+      <figure className="JobOffer" key={id}>
+        <picture className="JobOffer-image">
+          <img src={avatar} alt='This is an coming from robohash api'/>
+        </picture>
+        <figcaption className="JobOffer-form">
+          <h1 className="JobOffer-form-company">{last_name}</h1>
+          <p className="JobOffer-form-title">{employment.title}</p>
+          <p className="JobOffer-form-pres">{employment.key_skill}</p>
+          <p className="JobOffer-form-info" htmlFor="siret"><span>N° de siret : </span>{password}</p>
+          <p className="JobOffer-form-info" htmlFor="email"><span>Email : </span>{email}</p>
+          <p className="JobOffer-form-rgpd" >Veuillez verifier l'exactitude de vos données personnelles, ces dernière sont très importantes. Ces dernières serviront à établir et à éditer les contrats de travail de vos futurs employés.</p>
+          <Link to="/"  className="JobOffer-form-button"><button>Candidater</button></Link>
+        </figcaption>
+      </figure>
+    ))}
           </div>
         </div>
         
